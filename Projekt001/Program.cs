82d068f4f404ae32fe75230ac0f610f2e809fa26
Ivan Ristovski <rio01092001@gmail.com>
2023-11-25 03:23:43 +0100

    Add project files.

---
+++ Projekt001/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GettingInput
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Koliko imate godina");
+            string input = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(input))
+            {
+                Console.WriteLine("Ne ispravan unos, molim vas unesite vase godine!");
+                // Ne ispravan unos
+            }
+            else
+                Console.WriteLine($"Imate{input}godina");
+        }
+    }
+}
